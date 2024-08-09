7b096c043889dcf7d86ab6c72bbbdbba492e1dbd
salah samir <salahsaamir707@gmail.com>
2024-08-09 16:26:52 +0300

    finishing

--- src/components/ProductDetails/Details/ProductDetails.tsx
+++ src/components/ProductDetails/Details/ProductDetails.tsx
@@ -15,7 +15,7 @@ const ProductDetailsContent = ({ stock, data }: IProps) => {
     
   return (
     <main>
-        <h3 className="text-lg text-gray-600 font-semibold pt-20">{data.name}</h3>
+        <h3 className="text-lg text-gray-600 font-semibold pt-16 ">{data.name}</h3>
         <Stock stock={stock}  />
         <hr className='my-4 bg-slate-600' />
         <Details data={data} />
